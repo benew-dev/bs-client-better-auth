9119d031ffd733bb7a11eba75b52cc057c0274fb
Fathismael93 <fathismael@gmail.com>
2025-11-04 22:22:58 +0300

    Handling session updates

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -1,6 +1,5 @@
 "use client";
 
-import { refreshSession } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { createContext, useState } from "react";
 import { toast } from "react-toastify";
